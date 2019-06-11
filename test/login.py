8188c5eb852fb069e3a6ea62f1f6d6367eab70b5
经理 <manager@163.com>
2019-06-11 19:38:24 +0800

    0.1

--- test/login.py
+++ test/login.py
@@ -1,15 +1,6 @@
+
 a = 1
 
 def index():
 	print("index info")
-
-def index1():
-	print("index info")
-def index2()
-	print("index info")
-def index3 ():
-	print("index info")
-def index4 ():
-	print("index info")
-
 
